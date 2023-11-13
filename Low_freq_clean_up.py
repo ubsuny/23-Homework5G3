f27142e856c754f68e8ca1423cee55d8625aad55
uarif <uarif@users.noreply.github.com>
2023-11-13 02:58:23 -0500

    Update Low_freq_clean_up.py
    Cosmetics,

--- Low_freq_clean_up.py
+++ Low_freq_clean_up.py
@@ -1,23 +1,7 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from scipy.signal import butter, filtfilt
-
-# Define the butter_lowpass_filter function
-def butter_lowpass_filter(data, cutoff_frequency, sample_rate, order=4):
-  """Applies a low-pass filter to the given data using the scipy.signal.butter() and scipy.signal.filtfilt() functions.
-
-  Args:
-    data: A NumPy array containing the data to be filtered.
-    cutoff_frequency: The cutoff frequency of the filter in Hz.
-    sample_rate: The sample rate of the data in Hz.
-    order: The order of the filter.
-
-  Returns:
-    A NumPy array containing the filtered data.
-  """
-
-  return filtfilt(*butter(order, cutoff_frequency / (sample_rate / 2), btype='low'), data)
 
 # Load CO2 data using pandas
 url = "https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/txt/co2_mlo_surface-flask_1_ccgg_month.txt"
@@ -38,19 +22,22 @@ filtered_data = butter_lowpass_filter(co2_concentration, cutoff_frequency, sampl
 plt.figure(figsize=(10, 6))
 
 plt.subplot(2, 1, 1)
-plt.scatter(time, co2_concentration, label="Raw Data", s=5)  # 's' adjusts the size of the points
-plt.title("Raw CO2 Concentration(PPM) vs. Frequency/6months")
-plt.xlabel("Frequency/6 month")
-plt.ylabel("CO2 Concentration(PPM)")
+plt.scatter(time, co2_concentration, label="Raw Data", s=5, color='blue')  # Change the line color to blue
+plt.title("Raw CO2 Concentration (PPM) vs. Frequency (6 months)")
+plt.xlabel("Frequency (6 months)")
+plt.ylabel("CO2 Concentration (PPM)")
+plt.grid(True)
 plt.legend()
 
 plt.subplot(2, 1, 2)
-
-plt.scatter(time, filtered_data, label="Filtered Data", s=5, color='red')  # 's' adjusts the size of the points
-plt.title("Filtered CO2 Concentration(PPM) vs. Frequenccy/6months")
-plt.xlabel("Frequency/6 month")
-plt.ylabel("CO2 Concentration(PPM)")
+plt.scatter(time, filtered_data, label="Filtered Data", s=5, color='red')  # Change the line color to red
+plt.title("Filtered CO2 Concentration (PPM) vs. Frequency (6 months)")
+plt.xlabel("Frequency (6 months)")
+plt.ylabel("CO2 Concentration (PPM)")
+plt.grid(True)
 plt.legend()
 
 plt.tight_layout()
-plt.show()
+
+# Save the plot as a PNG image named `co2_concentration.png`
+plt.savefig('co2_concentration.png')
